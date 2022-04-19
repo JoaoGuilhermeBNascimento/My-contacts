fe18269270a954d5422d9e3d73c9198a347234c5
JoaoGuilhermeBNascimento <joaoguilhermebn@hotmail.com>
2022-04-19 00:23:28 -0300

    add routes

--- src/Components/ContactsLists/index.js
+++ src/Components/ContactsLists/index.js
@@ -1,6 +1,8 @@
-import { Container, Header, ListContainer } from "./styles";
+import { Container, Header, ListContainer, Card } from "./styles";
 
-import arrow from '../../Assets/images/icons/arrow.svg'
+import arrow from "../../Assets/images/icons/arrow.svg";
+import edit from "../../Assets/images/icons/edit.svg";
+import trash from "../../Assets/images/icons/trash.svg";
 
 export default function ContactsLists() {
   return (
@@ -17,6 +19,63 @@ export default function ContactsLists() {
             <img src={arrow} alt="arrow" />
           </button>
         </header>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>João Guilherme</strong>
+              <small>instagram</small>
+            </div>
+            <span>joaoguilhermebn@hotmail.com</span>
+            <span>(61) 99970-4636</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Mateus Silva</strong>
+              <small>instagram</small>
+            </div>
+            <span>mateus@devacademy.com.br</span>
+            <span>(41) 9999-9999</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>João Guilherme</strong>
+              <small>instagram</small>
+            </div>
+            <span>joaoguilhermebn@hotmail.com</span>
+            <span>(61) 99970-4636</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
       </ListContainer>
     </Container>
   );
